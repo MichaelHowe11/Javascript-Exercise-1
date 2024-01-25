209765fae76292d4d053013f8273430705a760fe
MichaelHowe11 <michael.howell23@student.cart.org>
2024-01-25 15:49:29 -0800

    awesome sauce

--- exercise.js
+++ exercise.js
@@ -1,36 +1,67 @@
-CalcAsk = prompt("Addition, Subtraction, Multiplication, or Division?");
-CalcAsk = CalcAsk.toLowerCase();
-let calcset = 0;
+function add() {
+    CalcAsk = prompt("Input two numbers you want to add and seperate them by a space.");
+    CalcAsk = CalcAsk.split(' ');
+    console.log(CalcAsk);
+    CalcAsk = CalcAsk.map(Number);
 
-
-if (CalcAsk === "addition") {
-    calcset = 0;
-} else if (CalcAsk === "subtraction") {
-    calcset = 1;
-} else if (CalcAsk === "multiplication") {
-    calcset = 2;
-} else if (CalcAsk === "division") {
-    calcset = 3;
-}
-
-CalcAsk = prompt("What Numbers do you want to add?");
-let output = 0;
-CalcAsk = CalcAsk.split(' ');
-let CalcAsk2 = CalcAsk.map(Number);
-
-console.log(CalcAsk2);
-
-for (let i = 0; i < CalcAsk2.length; i++) {
-    if (calcset === 0) {
-        output = CalcAsk2[i]+CalcAsk2[i-i];
-    } else if (calcset === 1) {
-    
-    } else if (calcset === 2) {
-    
-    } else if (calcset === 3) {
-    
+    for (let i = 0; i < CalcAsk.length; i++) {
+            output = CalcAsk[i-i]+CalcAsk[i];
     }
+    console.log(output);
+    document.getElementById("outputhtml").innerHTML = output;
 }
+function sub() {
+    CalcAsk = prompt("Input two numbers you want to subtract and seperate them by a space.");
+    CalcAsk = CalcAsk.split(' ');
+    CalcAsk = CalcAsk.map(Number);
+    for (let i = 0; i < CalcAsk.length; i++) {
+            output = CalcAsk[i-i]-CalcAsk[i];
+    }
+    if (isNaN(CalcAsk)) {
+    document.getElementById("outputhtml").innerHTML = "You inputted an invalid equation";
+    } else {
+    document.getElementById("outputhtml").innerHTML = output;
+    }
+    console.log(output);
+}
+function mult() {
+    CalcAsk = prompt("Input two numbers you want to multiply and seperate them by a space.");
+    CalcAsk = CalcAsk.split(' ');
+    CalcAsk = CalcAsk.map(Number);
+    for (let i = 0; i < CalcAsk.length; i++) {
+            output = CalcAsk[i-i]*CalcAsk[i];
+    }
+    console.log(output);
+    document.getElementById("outputhtml").innerHTML = output;
 
-console.log(output);
-
+}
+function division() {
+    CalcAsk = prompt("Input two numbers you want to divide and seperate them by a space.");
+    CalcAsk = CalcAsk.split(' ');
+    CalcAsk = CalcAsk.map(Number);
+    for (let i = 0; i < CalcAsk.length; i++) {
+            output = CalcAsk[i-i]/CalcAsk[i];
+    }
+    console.log(output);
+    document.getElementById("outputhtml").innerHTML = output;
+}
+function mod() {
+    CalcAsk = prompt("Input two numbers you want to modulo and seperate them by a space.");
+    CalcAsk = CalcAsk.split(' ');
+    CalcAsk = CalcAsk.map(Number);
+    for (let i = 0; i < CalcAsk.length; i++) {
+            output = CalcAsk[i-i]%CalcAsk[i];
+    }
+    console.log(output);
+    document.getElementById("outputhtml").innerHTML = output;
+}
+function exponents() {
+    CalcAsk = prompt("Input two numbers you want to exponent and seperate them by a space.");
+    CalcAsk = CalcAsk.split(' ');
+    CalcAsk = CalcAsk.map(Number);
+    for (let i = 0; i < CalcAsk.length; i++) {
+            output = CalcAsk[i-i]**CalcAsk[i];
+    }
+    console.log(output);
+    document.getElementById("outputhtml").innerHTML = output;
+}
