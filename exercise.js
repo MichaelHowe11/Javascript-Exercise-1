81e33f2b93524b680f30cf310e31b363586e496c
MichaelHowe11 <michael.howell23@student.cart.org>
2024-01-26 15:33:47 -0800

    user friendlyness

--- exercise.js
+++ exercise.js
@@ -1,27 +1,35 @@
 let calctype = 0;
 let calcText = "";
+let split = "";
+
 function awesome() {
 if (event.srcElement.id === "addButton") {
     calctype = 0;
     calcText = "add";
+    split1 = '+'
 } else if (event.srcElement.id === "subButton") {
     calctype = 1;
     calcText = "subtract";
+    split1 = '-'
 } else if (event.srcElement.id === "multiButton") {
     calctype = 2;
     calcText = "multiply";
+    split1 = '*'
 } else if (event.srcElement.id === "divButton") {
     calctype = 3;
     calcText = "divide";
+    split1 = '/'
 } else if (event.srcElement.id === "modButton") {
     calctype = 4;
     calcText = "modulo";
+    split1 = '%'
 } else {
     calctype = 5;
     calcText = "exponent";
+    split1 = '^'
 }
-CalcAsk = prompt("Input two numbers you want to " + calcText +  " and seperate them by a space.");
-CalcAsk = CalcAsk.split(' ');
+CalcAsk = prompt("Input two numbers you want to " + calcText +  " and seperate them by the " + calcText + " sign.");
+CalcAsk = CalcAsk.split(split1);
 CalcAsk = CalcAsk.map(Number);
 for (let i = 0; i < CalcAsk.length; i++) {
     if (calctype === 0) {
